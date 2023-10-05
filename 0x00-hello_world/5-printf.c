5dd6e27982f6de1a8ef381bfbf34a8092c9d4e28
N-Nkuta <nokulungankuta.com>
2023-10-05 14:02:54 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main fuction to print words exactly and add a new line
+ *
+ * Return: 0 whenever successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
