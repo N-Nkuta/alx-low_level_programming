8392fb0d3e2588d80d6893be6afd9de267563901
N-Nkuta <nokulungankuta.com>
2023-10-08 06:55:12 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing lowercase alphabets
+ *
+ * Return: to be 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
