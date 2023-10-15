c80a55a6f0cb75bd9d2bf477195f05643dfbefd4
N-Nkuta <nokulungankuta.com>
2023-10-15 12:18:39 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @a: interger to be computed
+ * Return: -a or a
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
