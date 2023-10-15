1aeb7f405c1e5efa2f048f6ce1d68be6e1b35e65
N-Nkuta <nokulungankuta.com>
2023-10-15 10:55:48 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lower case
+ *
+ * Return: to be 0
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+		_putchar(a);
+	_putchar('\n');
+
+}
