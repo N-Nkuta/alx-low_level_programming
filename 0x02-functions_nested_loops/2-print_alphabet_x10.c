1aea58d607961056a50cec6fbd1c7097a1fc0e52
N-Nkuta <nokulungankuta.com>
2023-10-15 11:15:33 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lower case 10x
+ *
+ * Return: to be 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 1 ; a <= 10 ; a++)
+	{
+		for (b = 'a' ; b <= 'z' ; b++)
+			_putchar(b);
+	}
+	_putchar('\n');
+}
