00e49591b560dd9b8f791b93aa145be324cacd25
N-Nkuta <nokulungankuta.com>
2023-10-16 11:47:15 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int a;
+	int count = 0;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+		count++;
+	for (a = 0 ; a < count / 2 ; a++)
+	{
+		char b;
+
+		b = s[a];
+		s[a] = s[count - 1 - a];
+		s[count - 1 - a] = b;
+	}
+}
