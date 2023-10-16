e48e33462ce8ea3fed29d6784296412edd2b7463
N-Nkuta <nokulungankuta.com>
2023-10-16 13:00:16 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - function that convert a string to an integer
+ * @s: string to be converted
+ * Return: number or 0
+ */
+
+int _atoi(char *s)
+{
+	char b[10];
+}
