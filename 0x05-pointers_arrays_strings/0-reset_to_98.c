6f704313b162f2f5466988970f2f9c4784b6cf50
N-Nkuta <nokulungankuta.com>
2023-10-16 10:41:39 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98 in a parameter
+ * @n: pointer that points to value that needs changing
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
