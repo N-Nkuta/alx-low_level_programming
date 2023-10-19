dac8df5cb196193c6492ac487e4a1526570e85fd
N-Nkuta <nokulungankuta.com>
2023-10-19 14:07:17 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: integer
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+	unsigned int u;
+
+	u = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		u = -n;
+	}
+	if (u / 10 != 0)
+	{
+		print_number(u / 10);
+	}
+	_putchar((u % 10) + '0');
+}
+
