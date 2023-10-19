5def3f0a48dd69053d9691773670b8c750802638
N-Nkuta <nokulungankuta.com>
2023-10-19 12:53:34 -0700

    6-cap.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - a function that capitalizes all words of a string
+ * @s: string to be capitalized
+ * Return: new string
+ */
+
+char *cap_string(char *s)
+{
+	int a;
+	int b;
+	char sp[13] = {' ', '\t', '\n', ',', ':', '.', '!', 
+'?', '"', '(', ')', '{', '}'};
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{
+		if (a == 0 && s[a] >= 'a' && s[a] <= 'z')
+			s[a] = s[a] - 32;
+		for (b = 0 ; b < 13 ; b++)
+		{
+			if (s[a] == sp[b])
+			{
+				if (s[a + 1] >= 'a' && s[a + 1] <= 'z')
+					s[a + 1] = s[a + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
