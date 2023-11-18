c81e5cdbebd75e7d7f51d2bb3383cc1316e5561d
N-Nkuta <nokulungankuta@gmail.com>
2023-11-18 12:14:50 -0800

    Pointers

---
+++ 0x08-recursion/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints strings
+ * @s: printed string
+ */
+
+void _puts_recursion(char *s)
+{
+        char s;
+
+        _putchar('s', \n);
+
+        returrn 0;
+}
