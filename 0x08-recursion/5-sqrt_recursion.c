8bf7249c67c81155d77144e0e03306817c915aaa
N-Nkuta <nokulungankuta@gmail.com>
2023-11-19 12:29:09 -0800

    6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,4 +8,29 @@
 
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursion(n));
+}
+
+/**
+ * _functional_sqrt_recursion - recursive function
+ * @n: number to be square rooted
+ * @a: number to be compared
+ * Return: square root
+ */
+
+int _functional_sqrt_recursion(int n, int a)
+{
+	if (a * a > n)
+	{
+		return (-1);
+	}
+	if (a * a == n)
+	{
+		return (a);
+	}
+		return (_sqrt_recursion(n + 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _functional_sqrt_recursion(int n, int a);
 int is_prime_number(int n);
+int functional_prime(int n, int a);
 
 #endif
