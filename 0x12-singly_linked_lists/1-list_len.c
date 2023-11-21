45c4d89807c3be97c9b62687a469eed4600537bc
N-Nkuta <nokulungankuta@gmail.com>
2023-11-21 08:55:57 -0800

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: head node
+ * Return: length of node
+ */
+
+
+size_t list_len(const list_t *h)
+{
+	size_t  elements = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
